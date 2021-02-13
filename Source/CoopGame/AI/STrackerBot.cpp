100f6368ac52f56ebefe00567b90b3c118beed6c
goatryder <nova9ok@gmail.com>
2021-02-13 21:18:04 +0200

    -tracker bot move to next path point, on reach get next path point -tracker bot on stuck add launch impulse

--- Source/CoopGame/AI/STrackerBot.cpp
+++ Source/CoopGame/AI/STrackerBot.cpp
@@ -15,8 +15,21 @@ ASTrackerBot::ASTrackerBot()
 	PrimaryActorTick.bCanEverTick = true;
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
-	RootComponent = MeshComp;
 	MeshComp->SetCanEverAffectNavigation(false);
+	MeshComp->SetSimulatePhysics(true);
+	RootComponent = MeshComp;
+
+	// defaults move
+	bUseVelocityChange = false;
+	MovementForce = 1000.0f;
+	RequiredDistanceToTarget = 100.0f;
+	MoveControlTick = 0.1f;
+
+	// defauls launch on stuck
+	bUseLaunchWhenStuck = true;
+	LaunchOnStuckImpulse = FVector(100.0f, 0.0f, 1000.0f);
+	TimeToConsiderStuck = 1.0f;
+	StuckDistanceDelta = 50.0f;
 }
 
 // Called when the game starts or when spawned
@@ -24,19 +37,23 @@ void ASTrackerBot::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	GetNextPathPoint();
+	// Find initial move to
+	NextPathPoint = GetNextPathPoint();
+
+	// Move Tick Enable
+	GetWorldTimerManager().SetTimer(TimerHandle_MoveControl, this, &ASTrackerBot::OnTimer_MoveControl, MoveControlTick, true);
 }
 
 
 FVector ASTrackerBot::GetNextPathPoint()
 {
 	// Hack, to get player location
-	AActor* PlayerPawn  = Cast<AActor>(UGameplayStatics::GetPlayerCharacter(this, 0));
+	ACharacter* PlayerPawn = UGameplayStatics::GetPlayerCharacter(this, 0);
 
-	/*UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);*/
+	UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
 
 	FVector NextPoint;
-	/*
+	
 	if (NavPath->PathPoints.Num() > 1)
 	{
 		// next point in the path
@@ -49,9 +66,138 @@ FVector ASTrackerBot::GetNextPathPoint()
 	}
 
 	// debug
-	DrawDebugSphere(GetWorld(), NextPoint, 100.0, 12, FColor::Cyan, true, 1.0f);*/
+	DrawDebugSphere(GetWorld(), NextPoint, 100.0, 12, FColor::Cyan, false, 3.0f);
 
 	return NextPoint;
+}
+
+
+bool ASTrackerBot::IsStuckDesisionMake()
+{
+	float StuckDistanceMeasure = 0.0f;
+
+	if (bPossiblyStuck)
+	{
+		// check actual possible stuck location
+		StuckDistanceMeasure = (GetActorLocation() - PossibleStuckLocation).Size();
+		
+		// debug
+		DrawDebugString(GetWorld(), GetActorLocation() + FVector(0.0f, 0.0f, 100.0f), 
+			FString::Printf(TEXT("Delta PossibleStuckLocation: %s,  CountDown TimeToDesideStuck: %s"), 
+				*FString::SanitizeFloat(StuckDistanceMeasure), *FString::SanitizeFloat(TimeToConsiderStuckAccomulation)), (AActor*)0, FColor::Red, MoveControlTick);
+		//
+	}
+	else
+	{
+		// try init new stuck location
+		StuckDistanceMeasure = (GetActorLocation() - StuckCheck_PrevActorLocation).Size();
+		
+		// debug
+		DrawDebugString(GetWorld(), GetActorLocation() + FVector(0.0f, 0.0f, 100.0f),
+			FString::Printf(TEXT("Delta PrevLocation: %s,  CountDown TimeToDesideStuck: %s"),
+				*FString::SanitizeFloat(StuckDistanceMeasure), *FString::SanitizeFloat(TimeToConsiderStuckAccomulation)), (AActor*)0, FColor::Orange, MoveControlTick);
+		//
+	}
+
+	// check if possibly stuck
+	if (StuckDistanceMeasure <= StuckDistanceDelta)
+	{
+		if (!bPossiblyStuck)  // save possible stuck location only once
+		{
+			PossibleStuckLocation = StuckCheck_PrevActorLocation;
+			bPossiblyStuck = true;
+		}
+
+		// timer to make final stuck verdict
+		TimeToConsiderStuckAccomulation += MoveControlTick;
+
+		if (TimeToConsiderStuckAccomulation >= TimeToConsiderStuck)
+		{
+			TimeToConsiderStuckAccomulation = 0.0f;  // block multiple execution
+			return true;
+		}
+	}
+	else
+	{
+		TimeToConsiderStuckAccomulation = 0.0f;
+		bPossiblyStuck = false;
+	}
+
+	return false;
+}
+
+
+void ASTrackerBot::LaunchOnStuck()
+{
+
+	FVector Direction = NextPathPoint - GetActorLocation();
+	Direction.Normalize();
+	FVector ImpulseTowardsNextPathPoint = Direction.Rotation().RotateVector(LaunchOnStuckImpulse);
+
+	MeshComp->AddImpulse(ImpulseTowardsNextPathPoint, NAME_None, bUseVelocityChange);
+
+	// debug
+	DrawDebugDirectionalArrow(GetWorld(), GetActorLocation(), GetActorLocation() + ImpulseTowardsNextPathPoint, 32, FColor::Blue, false, 3.0f, 0, 2.0f);
+}
+
+
+void ASTrackerBot::HandleStuck_OnTimer_MoveControl()
+{
+	if (bUseLaunchWhenStuck)
+	{
+		if (IsStuckDesisionMake())
+		{
+			LaunchOnStuck();
+		}
+		else
+		{
+			// update prev actor location
+			StuckCheck_PrevActorLocation = GetActorLocation();
+		}
+	}
+}
+
+
+void ASTrackerBot::UpdateNextPathPoint_WhenReach()
+{
+	float DistanceToTarget = (GetActorLocation() - NextPathPoint).Size();
+
+	if (DistanceToTarget <= RequiredDistanceToTarget)
+	{
+		// Calc new path point to move to
+		NextPathPoint = GetNextPathPoint();
+
+		// debug
+		DrawDebugString(GetWorld(), GetActorLocation(), FString::Printf(TEXT("Reached! Next Target Point: %s"), *NextPathPoint.ToString()), (AActor*)0, FColor::Cyan, MoveControlTick, true);
+	}
+}
+
+
+void ASTrackerBot::OnTimer_MoveControl()
+{
+	// check
+	if (!MeshComp->IsSimulatingPhysics())
+		UE_LOG(LogTemp, Warning, TEXT("ASTrackerBot::MoveToNextPathPoint(), MeshComp Physics Simulation is disabled! Please, enable"));
+
+	// check if NextPathPoint reached then update NextPathPoint
+	UpdateNextPathPoint_WhenReach();
+
+	// check if stuck then launch
+	HandleStuck_OnTimer_MoveControl();
+}
+
+
+void ASTrackerBot::OnTick_MoveToNextPathPoint()
+{
+	// Keep moving towards next target
+	FVector ForceDirection = NextPathPoint - GetActorLocation();
+	ForceDirection.Normalize();
+	ForceDirection *= MovementForce;
+
+	MeshComp->AddForce(ForceDirection, NAME_None, bUseVelocityChange);
+
+	// debug
+	DrawDebugDirectionalArrow(GetWorld(), GetActorLocation(), GetActorLocation() + ForceDirection, 32, FColor::Yellow, false, 0.0f, 0, 2.0f);
 }
 
 
@@ -59,5 +205,5 @@ FVector ASTrackerBot::GetNextPathPoint()
 void ASTrackerBot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	OnTick_MoveToNextPathPoint();
 }
--- Source/CoopGame/AI/STrackerBot.h
+++ Source/CoopGame/AI/STrackerBot.h
@@ -22,7 +22,73 @@ protected:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
 		UStaticMeshComponent* MeshComp;
 	
+	// [Tracker bot Move resolving logic] ////////////////////////////////////////////////////////////////////////////////
+
 	FVector GetNextPathPoint();
+	
+	// Next point in navigation path
+	FVector NextPathPoint;
+
+	/** Force Applied To Mesh Comp Every MoveTick */
+	UPROPERTY(EditDefaultsOnly, Category = "Tracker Bot")
+		float MovementForce;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tracker Bot")
+		bool bUseVelocityChange;
+	
+	/** Allowed Distance Inaccuracy to consider NextPathPoint reached */
+	UPROPERTY(EditDefaultsOnly, Category = "Tracker Bot")
+		float RequiredDistanceToTarget;
+
+	/** Delay when MoveToNextPathPoint will be called */
+	UPROPERTY(EditDefaultsOnly, Category = "Tracker Bot")
+		float MoveControlTick;
+
+	FTimerHandle TimerHandle_MoveControl;
+
+	void UpdateNextPathPoint_WhenReach();
+
+	void OnTimer_MoveControl();
+
+	void OnTick_MoveToNextPathPoint();
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// [Tracker bot Stuck resolving logic] ///////////////////////////////////////////////////////////////////////////////
+
+	/** When TracketBot stuck somewhere it will be launched to NextPathPoint with LaunchOnStuckImpulse*/
+	UPROPERTY(EditDefaultsOnly, Category = "Tracket Bot")
+		bool bUseLaunchWhenStuck;
+
+	/** X vector val here is towards direction launch impulse to NextPathPoint */
+	UPROPERTY(EditDefaultsOnly, Category = "Tracket Bot", meta = (EditCondition = "bUseLaunchWhenStuck"))
+		FVector LaunchOnStuckImpulse;
+
+	/** When TrackerBot stays in StuckDistanceDelta range that amount of time - it will recieve LaunchOnStuckImpulse*/
+	UPROPERTY(EditDefaultsOnly, Category = "Tracker Bot", meta = (EditCondition = "bUseLaunchWhenStuck"))
+		float TimeToConsiderStuck;
+
+	float TimeToConsiderStuckAccomulation;
+
+	/** If TrackerBot stayes in this range for TimeToConsiderStuck - it will be recieve LaunchOnStuckImpulse*/
+	UPROPERTY(EditDefaultsOnly, Category = "Tracker Bot", meta = (EditCondition = "bUseLaunchWhenStuck"))
+		float StuckDistanceDelta;
+
+	/* Last MoveTick Actor Location (used for initialization tracker bot possible stuck location) */
+	FVector StuckCheck_PrevActorLocation;
+
+	FVector PossibleStuckLocation;
+
+	/** bool out val from IsStuckDesisionMake() */
+	bool bPossiblyStuck;
+
+	void HandleStuck_OnTimer_MoveControl();
+
+	bool IsStuckDesisionMake();
+
+	void LaunchOnStuck();
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 public:	
 	// Called every frame
